2b5c7962656a9b6da7aa9f369cb93c81133e7d16
Sergio Filho <sergiolpf@gmail.com>
2024-09-11 17:44:10 +1200

    implementacao do exercicio same tree

---
+++ 8_Trees/100SameTree/sameTree.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+	//[1,2,2,3,3,null,null,4,4]
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 3,
+				Left: &TreeNode{
+					Val: 4,
+				},
+				Right: &TreeNode{
+					Val: 4,
+				},
+			},
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+		Right: &TreeNode{
+			Val: 2,
+		},
+	}
+	fmt.Println(isSameTree(root, root))
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if (p != nil && q == nil) ||
+		p == nil && q != nil {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+
+}
