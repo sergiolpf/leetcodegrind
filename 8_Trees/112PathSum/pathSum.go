604c6f57216ee5625778e1cb1a623646e050249e
Sergio Filho <sergiolpf@gmail.com>
2024-09-12 08:28:33 +1200

    implementacao do exercicio Path Sum

---
+++ 8_Trees/112PathSum/pathSum.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func main() {
+	//[4,2,7,1,3,6,9]
+	root := &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val: 11,
+				Left: &TreeNode{
+					Val: 7,
+				},
+				Right: &TreeNode{
+					Val: 2,
+				},
+			},
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+		Right: &TreeNode{
+			Val: 7,
+			Left: &TreeNode{
+				Val: 6,
+			},
+			Right: &TreeNode{
+				Val: 9,
+			},
+		},
+	}
+
+	fmt.Println(hasPathSum(root, 22))
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil {
+		if root.Val == targetSum {
+			return true
+		} else {
+			return false
+		}
+
+	}
+
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
